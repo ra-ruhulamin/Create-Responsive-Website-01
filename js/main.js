ac6b65493f46a2830e373dda4e300e8d67217d14
ra-ruhulamin <87590413+ra-ruhulamin@users.noreply.github.com>
2021-08-14 14:07:59 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+// open toggle in dropdown
+
+document.getElementsByClassName('open_menu')[0].addEventListener('click',function () {
+    document.getElementById('menuMd').setAttribute('style','width: 250px')
+    
+
+});
+document.getElementsByClassName('close_menu')[0].addEventListener('click',function () {
+    document.getElementById('menuMd').setAttribute('style','width: 0px')
+});
+
+// close toggle in dropdown
+
+
+
+
+
+    // AOS Instance
+    AOS.init();
+
+
+
+
+
+
+
+    Link In descriptions
